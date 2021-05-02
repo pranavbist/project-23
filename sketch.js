66b10ad669e88508453231266797a37fc1d861df
pranavbist <74127408+pranavbist@users.noreply.github.com>
2021-05-02 16:08:40 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -44,7 +44,7 @@ function setup() {
 	
 
 	//Create a Ground
-	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+	ground = Bodies.rectangle(width/2, 640, width, 10 , {isStatic:true} );
  	World.add(world, ground);
 
 
